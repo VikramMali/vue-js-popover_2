10f143cc33da35d95a00f8ad4db1d279d20ca29d
Webolement <email@vikrammali.in>
2019-05-13 15:06:16 +0530

    Update tooltip.js

--- tooltip.js
+++ tooltip.js
@@ -1,6 +1,7 @@
 // UI Page Profile #187
 //import rcrm_import from "/rcrm_import.js"
 Vue.component('inline-edit', {
+//export default {
     name: 'inline-edit',
     props: {
         'value': {
@@ -32,9 +33,11 @@ Vue.component('inline-edit', {
             openOnFocus: true,
             keepFirst: true,
             column_selected_options: '',
+            data_value: '',
             asyc_data: [],
             asyc_value: '',
             tooltip_class: 'is-white',
+            isFetching: false,
         }
     },
     watch: {
@@ -49,6 +52,7 @@ Vue.component('inline-edit', {
     components: {
         'rcrmdatepicker': () => rcrm_import('controls/rcrm_datepicker'),
         'fileupload': () => rcrm_import('controls/file_upload'),
+        'multiselect': () => rcrm_import('controls/multiselect'),
     },
     methods: {
         getAsyncData: _.debounce(function () {
@@ -160,7 +164,9 @@ Vue.component('inline-edit', {
     },
     mounted() {
         this.update_field = this.value[this.update_column.field];
-        
+        if (this.update_column.service) {
+            this.getAsyncDataOnLoad(this.update_column.service);
+        }
         /*
         for (var item in this.$modal.columns) {
             if (!this.$modal.columns[item].lock_bulk_editable) {
@@ -184,7 +190,6 @@ Vue.component('inline-edit', {
             })
         }
     },
-
     template: `
     <div class="field rcrm-inline">
         <p v-if="editstyle == 'popover'" v-tooltip.notrigger="{ html: 'popover'+$vnode.key, class: 'is-white'+toottipclass, visible: (visibility && edetable)}">
@@ -312,10 +317,8 @@ Vue.component('inline-edit', {
             <form style="display: inherit;" v-on:submit.prevent="updateFields()">
                 <b-field :type="errors.has(update_column.label) ? 'is-danger': ''" :label="update_column.label"
                     :message="errors.has(update_column.label) ? errors.first(update_column.label) : ''">
-
-                    <b-input v-if="update_column.type == ''" id="sTests-" :name="update_column.label"
-                        v-validate="'required|min:3'" type="text" value="" maxlength="40"
-                        placeholder="Select Field Name First">
+                    <b-input v-if="update_column.type == ''" id="sTests-" :name="update_column.label" v-validate="'required|min:3'"
+                        type="text" value="" maxlength="40" placeholder="Select Field Name First">
                     </b-input>
 
                     <b-input v-if="update_column.type == 'text'" v-model="update_field" id="sTest-companyText"
@@ -327,63 +330,52 @@ Vue.component('inline-edit', {
                         type="number" value="" maxlength="40" :name="update_column.label" placeholder="Enter Here"
                         v-validate="'required|min_value:0'">
                     </b-input>
-
-                    <b-autocomplete
-                        v-if="update_column.type == 'dropdown' && update_column.data && update_column.field.indexOf('custcolumn') !== -1 "
-                        icon="chevron-down" v-model="column_selected_options" placeholder="Select Value"
-                        :name="update_column.label" class="is-icon-right" :keep-first="keepFirst"
-                        :open-on-focus="openOnFocus" :data="filteredCustomObj(update_column.data)"
-                        :field="update_column.dropdown_field" @select="option => update_field = option"
-                        v-validate="{ rules: { required: ( update_field != '' ) ? false : true} }">
+                    <!-- Custom fields of type Drop down -->
+                    <b-autocomplete v-if="update_column.type == 'dropdown'  && update_column.field.indexOf('custcolumn') !== -1 "  
+                        icon="chevron-down" v-model="data_value" placeholder="Select Value" :name="update_column.label"
+                        class="is-icon-right" :keep-first="keepFirst" :open-on-focus="openOnFocus" :data="filteredCustomObj(update_column.data)"
+                        :field="update_column.dropdown_field" @select="option => update_field = option" v-validate="{ rules: { required: ( update_field != '' ) ? false : true} }">
+                    </b-autocomplete>
+                    <!-- Custom fields Drop down ends -->
+                    <b-autocomplete v-else-if="update_column.type == 'dropdown' && update_column.data"
+                        icon="chevron-down" v-model="data_value" placeholder="Select Value" :name="update_column.label"
+                        class="is-icon-right" :keep-first="keepFirst" :open-on-focus="openOnFocus" :data="filteredCustomObj(update_column.data)"
+                        :field="update_column.dropdown_field" @select="option => update_field = option.id" v-validate="{ rules: { required: ( update_field != '' ) ? false : true} }">
                     </b-autocomplete>
 
-                    <b-autocomplete
-                        v-else-if="update_column.type == 'dropdown' && update_column.data && ( update_column.field == 'industryid' || update_column.field == 'qualificationid' || update_column.field == 'stageid') "
-                        icon="chevron-down"
-                        :placeholder="update_column.field == 'stageid' ? 'Select Contact stage' : 'Select Value'"
-                        :name="update_column.label" v-model="column_selected_options" class="is-icon-right"
-                        :keep-first="keepFirst" :open-on-focus="openOnFocus" :data="filteredColumnsObj(update_column.data)"
-                        :field="update_column.dropdown_field" @select="option => update_field = option.id"
-                        v-validate="{ rules: { required: ( column_selected_options != '' ) ? false : true} }">
-                    </b-autocomplete>
-
-                    <b-autocomplete
-                        v-else-if="update_column.type == 'dropdown' && update_column.data && update_column.field == 'ownerid' && update_column.label == 'Owner'"
-                        icon="chevron-down" placeholder="Select Value" :name="update_column.label" v-model="owner_"
-                        class="is-icon-right" :keep-first="keepFirst" :open-on-focus="openOnFocus"
-                        :data="filteredOwnerObj(update_column.data)" :field="update_column.dropdown_field"
-                        @select="option => update_field = option.id"
-                        v-validate="{ rules: { required: ( column_selected_options != '' ) ? false : true} }">
-                    </b-autocomplete>
-
-                    <b-autocomplete
-                        v-else-if="update_column.type == 'dropdown' && update_column.data && update_column.field.indexOf('custcolumn') === -1 "
-                        icon="chevron-down" placeholder="Select Value" class="is-icon-right" :keep-first="keepFirst"
-                        :open-on-focus="openOnFocus" :data="update_column.data" :field="update_column.dropdown_field"
-                        :name="update_column.label"
-                        v-validate="{ rules: { required: ( update_field != '' ) ? false : true} }"
-                        @select="option => update_field = option.id">
-                    </b-autocomplete>
-
+                    <!-- Custom fields Multi Select Ends -->
+                    <multiselect v-else-if="update_column.type == 'multiselect' && update_column.data"
+                        :key="update_field"
+                        :data="update_column.data"
+                        :value="update_field"
+                        @input="update_field = $event">
+                    </multiselect>
+                    <!-- Custom fields Multi Select Ends -->
                     <b-autocomplete v-else-if="update_column.type == 'dropdown' && update_column.service"
-                        placeholder="search by service" :open-on-focus="openOnFocus" v-model="asyc_value"
-                        :field="update_column.dropdown_field" :name="update_column.label" :loading="is_loading"
+                        placeholder="Start Typing" :open-on-focus="openOnFocus" v-model="asyc_value" :field="update_column.dropdown_field"
+                        :name="update_column.label" :loading="isFetching" v-validate="{ rules: { required: ( update_field != '' ) ? false : true} }"
                         :data="asyc_data" @keyup.native="getAsyncData" @select="option => update_field = option.id">
                     </b-autocomplete>
+
+                    <rcrmdatepicker v-else-if="update_column.type == 'date'" v-model="update_field" id="sTest-companyDate"
+                        placeholder="Click to select date..." icon="calendar-today" :name="update_column.label"
+                        v-validate="'required|min:0'">
+                    </rcrmdatepicker>
+
+                    <b-switch v-else-if="update_column.type == 'availability'" v-model="update_field" true-value="1"
+                        false-value="0" id="sTest-companySwitch">
+                        {{ update_field == 1 ? 'Yes' : 'No' }} 
+                    </b-switch>
+
+                    <b-switch v-else-if="update_column.type == 'checkbox'" v-model="update_field" true-value="1"
+                        false-value="0">
+                        {{ update_field == 1 ? 'Yes' : 'No' }} 
+                    </b-switch>
 
                     <fileupload v-else-if="update_column.type == 'file'" v-model="update_field"
                         :accountid="$root.user.accountid" :slug="value.slug" :folder="update_column.entity"
                         :column="update_column.field">
                     </fileupload>
-
-                    <rcrmdatepicker v-if="update_column.type == 'date'" v-model="update_field" id="sTest-companyDate"
-                        placeholder="Click to select date..." icon="calendar-today" :name="update_column.label"
-                        v-validate="'required|min:0'">
-                    </rcrmdatepicker>
-
-                    <b-switch v-if="update_column.type == 'availability'" v-model="update_field" true-value="1"
-                        false-value="0" id="sTest-companySwitch">
-                        {{ update_field == 1 ? 'Yes' : 'No' }} </b-switch>
                 </b-field>
                 <div class="inline-actions">
                     <button class="button" :class="{'is-loading' : is_loading}"><i
